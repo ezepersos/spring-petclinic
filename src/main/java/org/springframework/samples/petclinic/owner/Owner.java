bbc393f5c35ff5445f8f4aae9dffe9a37e1169d5
ezepersos <ezequielperez50@gmail.com>
2021-11-04 12:44:37 +0100

    Commit de ejemplo
    - Modificado X
    
    - Modificado A
    
    - Añadido B

--- src/main/java/org/springframework/samples/petclinic/owner/Owner.java
+++ src/main/java/org/springframework/samples/petclinic/owner/Owner.java
@@ -111,11 +111,7 @@ public class Owner extends Person {
 		pet.setOwner(this);
 	}
 
-	/**
-	 * Return the Pet with the given name, or null if none found for this Owner.
-	 * @param name to test
-	 * @return true if pet name is already in use
-	 */
+
 	public Pet getPet(String name) {
 		return getPet(name, false);
 	}
